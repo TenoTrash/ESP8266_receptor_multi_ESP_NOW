1053c581e9961b6754f8bb0a1faa5315870d4795
Marcelo Ferrarotti <38758218+TenoTrash@users.noreply.github.com>
2021-09-09 18:33:23 -0300

    Código para los emisores

---
+++ ESP_NOW_Multi_Sender_ID_1.ino
@@ -0,0 +1,78 @@
+/*
+  Basado en el ejemplo de Rui Santos https://RandomNerdTutorials.com/esp-now-many-to-one-esp8266-nodemcu/
+
+  Módulo emsisor de que envía dos variables int random usando ESP-NOW
+
+   Marcelo Ferrarotti
+  2021
+*/
+
+#include <ESP8266WiFi.h>
+#include <espnow.h>
+
+// MAC Address del receptor --->>>
+uint8_t broadcastAddress[] = {0x84, 0xF3, 0xEB, 0x58, 0x1C, 0xE0};
+
+// Set your Board ID (ESP32 Sender #1 = BOARD_ID 1, ESP32 Sender #2 = BOARD_ID 2, etc)
+#define BOARD_ID 1
+
+// Este struct es igual al del receptor
+typedef struct struct_message {
+  int id;
+  int x;
+  int y;
+} struct_message;
+
+struct_message myData;
+
+unsigned long lastTime = 0;
+unsigned long timerDelay = 10000;
+
+// Callback when data is sent
+void OnDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
+  Serial.print("\r\nLast Packet Send Status: ");
+  if (sendStatus == 0) {
+    Serial.println("Delivery success");
+  }
+  else {
+    Serial.println("Delivery fail");
+  }
+}
+
+void setup() {
+  // Init Serial Monitor
+  Serial.begin(115200);
+
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+
+  // Init ESP-NOW
+  if (esp_now_init() != 0) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  // Set ESP-NOW role
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+
+  // Once ESPNow is successfully init, we will register for Send CB to
+  // get the status of Trasnmitted packet
+  esp_now_register_send_cb(OnDataSent);
+
+  // Register peer
+  esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+
+}
+
+void loop() {
+  if ((millis() - lastTime) > timerDelay) {
+    // Set values to send
+    myData.id = BOARD_ID;
+    myData.x = random(1, 50);
+    myData.y = random(1, 50);
+
+    // Send message via ESP-NOW
+    esp_now_send(0, (uint8_t *) &myData, sizeof(myData));
+    lastTime = millis();
+  }
+}
